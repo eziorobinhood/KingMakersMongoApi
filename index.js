c430b4f448d112ff993f6f1ce1e924bfac2f37cd
Rahuram Krishna <59604313+eziorobinhood@users.noreply.github.com>
2026-01-19 12:20:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 const PORT = 4000;
-const mongoURI = "mongodb+srv://rrkrish123_db_user:admin@kingmakerscluster.5t6zmad.mongodb.net/?appName=kingmakerscluster";
+const mongoURI = "mongodb+srv://rrkrish123_db_user:admin@kingmakerscluster.5t6zmad.mongodb.net/?appName=kingmakerscluste";
 
 
 mongoose.connect(mongoURI).then(() => {
